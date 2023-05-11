4a4bb252925f596a7b35255921dad925d04d806f
Yegor Shuvayev <77032216+YegorShuvayev@users.noreply.github.com>
2023-05-12 00:49:35 +1200

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
-load_dotenv()
+#load_dotenv()
 DJANGO_SECRET = os.getenv('DJANGO_SECRET')
 DJANGO_DB_NAME = os.getenv('DJANGO_DB_NAME')
 
